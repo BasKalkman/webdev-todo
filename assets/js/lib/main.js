514e55c76bf69b3c6de4e7f147001d68be0c0319
Bas K <39069678+Basje12@users.noreply.github.com>
2018-08-28 15:27:46 +0200

    JS functionality

--- assets/js/lib/main.js
+++ assets/js/lib/main.js
@@ -1,4 +1,23 @@
 // Check off todo by clicking
-$("li").click(function() {
-    $(this).toggleClass("completed");
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
+});
+
+// Remove todo when remove button is clicked'
+$("ul").on("click", "span", function(e) {
+  e.stopPropagation();
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+});
+
+// Create new todo
+$('input[type="text"]').keypress(function(e) {
+  if (e.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append(`<li><span>X</span> ${todoText}</li>`);
+  }
 });
