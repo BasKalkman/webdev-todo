2874daa1d0b0f82d47e9394f396bebe23d9ec112
Bas K <39069678+Basje12@users.noreply.github.com>
2018-08-29 11:40:37 +0200

    Save and initialize from localstorage

--- assets/js/lib/main.js
+++ assets/js/lib/main.js
@@ -11,6 +11,7 @@ $("ul").on("click", "span", function(e) {
     .fadeOut(500, function() {
       $(this).remove();
     });
+  updateLocalStorage();
 });
 
 // Create new todo
@@ -21,9 +22,35 @@ $('input[type="text"]').keypress(function(e) {
     $("ul").append(
       `<li> <span><i class="fas fa-trash-alt"></i></span> ${todoText}</li>`
     );
+    updateLocalStorage();
   }
 });
 
 $(".fa-plus").click(function() {
   $("input[type='text']").fadeToggle();
 });
+
+// Localstorage functions
+function updateLocalStorage() {
+  var todoItems = document.querySelectorAll("li");
+  var myTodoList = [];
+  todoItems.forEach(function(e) {
+    let item = e.textContent.replace(/^\s/, "");
+    myTodoList.push(item);
+  });
+
+  localStorage.setItem("myTodoList", JSON.stringify(myTodoList));
+}
+
+function loadPage() {
+  var getItems = localStorage.getItem("myTodoList");
+  var items = JSON.parse(getItems);
+  console.log(items);
+  items.forEach(function(e) {
+    $("ul").append(
+      `<li> <span><i class="fas fa-trash-alt"></i></span> ${e}</li>`
+    );
+  });
+}
+
+loadPage();
